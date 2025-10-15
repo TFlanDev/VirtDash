41eaf8414f3856519bc881246966cdcbcca69a4a
Thomas <Tdflan90@gmail.com>
2025-10-14 20:15:03 -0700

    feat(api): add endpoints to retrieve individual and all VM information

--- app/api.py
+++ app/api.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
-import hypervisor
+import hypervisor,schemas
+from typing import List
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-@app.get("/vms")
+@app.get("/vms", response_model=List[schemas.VM])
 def list_vms():
-    pass
+    hypervisor_conn = hypervisor.connect()
+    vms = hypervisor.list_vms(hypervisor_conn)
+    hypervisor.disconnect(hypervisor_conn)
+    return vms
+
+
+@app.get("/vm/{vmname}/", response_model=schemas.VM)
+def get_vm_by_name(vmname : str):
+    hypervisor_conn = hypervisor.connect()
+    vm = hypervisor.get_vm_info(hypervisor_conn, vmname)
+    hypervisor.disconnect(hypervisor_conn)
+    return vm
